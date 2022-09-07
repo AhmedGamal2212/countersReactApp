fde88556a1f3dd973e35a7699fea9420b0e54da8
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-09-07 19:23:37 +0200

    feat: Add a message when there are no counters found

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import Counter from "./counter";
 
 const Counters = ({counters, onDelete, onIncrement, onReset, onSingleReset, addCounter}) => {
+    const listOfCounters = counters.map(counter =>
+        <Counter 
+            key={counter.id} 
+            counter={counter} 
+            onDelete={onDelete} 
+            onIncrement={onIncrement} 
+            singleReset={onSingleReset} 
+        />);
     return ( 
         <div className="mx-auto">       
             <button className="btn btn-primary btn-sm m-2" 
                 onClick={onReset}>Reset All</button>   
             <button className="btn btn-secondary btn-sm m-2"
                 onClick={addCounter}>Add a counter</button>
-            {counters.map(counter =>
-                <Counter 
-                    key={counter.id} 
-                    counter={counter} 
-                    onDelete={onDelete} 
-                    onIncrement={onIncrement} 
-                    singleReset={onSingleReset} 
-                />)}
+            
+            {listOfCounters.length ? listOfCounters : <h1>No counters! please add one</h1>}
         </div>
      );
 }
